51e5f1563b348e72821381fa0b0dc5099e040a2d
Vinícius M Lima <viniciusmendeslima@yahoo.com>
2025-07-16 16:29:58 -0300

    função de pesquisa por tipo em andamento

--- src/services/fetchPokemonByType.js
+++
@@ -1,5 +0,0 @@
-export async function fetchPokemonByType(typeUrl) {
-  const res = await fetch(typeUrl);
-  const data = await res.json();
-  return data.pokemon.map((p) => p.pokemon); // retorna array de pokémons
-}
--- src/services/fetchPokemonData.js
+++
@@ -1,5 +0,0 @@
-export async function fetchPokemonData() {
-  const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=10");
-  const data = await res.json();
-  return data.results;
-}
